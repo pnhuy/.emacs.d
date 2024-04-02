2845ca002bb9e811743d1b87b57e3c5bbf21b2ba
Huy Pham <keixi512@gmail.com>
2024-04-02 13:50:53 +0700

    Reconfigure web-mode for typescript

--- languages/web.el
+++ languages/web.el
@@ -1,25 +1,47 @@
-(require 'prettier-js)
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1)
+  (define-key web-mode-map (kbd "M-.") 'tide-jump-to-definition)
+  (define-key web-mode-map (kbd "M-?") 'tide-references)
+  (define-key web-mode-map (kbd "M-;") 'tide-rename-symbol)
+)
 
 (use-package web-mode
   :ensure t
+  :init
   :mode (("\\.html?\\'" . web-mode)
          ("\\.css\\'" . web-mode)
-         ("\\.js\\'" . web-mode))
+         ("\\.jsx?\\'" . web-mode)
+         ("\\.tsx?\\'" . web-mode))
+  :hook
+  (web-mode . prettier-js-mode)
+  (web-mode . company-mode)
+  (web-mode . flycheck-mode)
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+  (js-indent-level 2)
+  (web-mode-enable-auto-pairing t)
+  (web-mode-enable-auto-opening t)
+  (web-mode-content-types-alist '(("jsx"  . "\\.js[x]?\\'")))
   :config
-  (add-hook 'web-mode-hook 'prettier-js-mode)
-  (add-hook 'web-mode-hook 'company-mode)
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-enable-auto-pairing t
-        web-mode-enable-auto-opening t
-        )
-  (setq web-mode-content-types-alist '(("jsx"  . "\\.js[x]?\\'")))
+  (require 'prettier-js)
+  (require 'flycheck)
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-add-mode 'typescript-tslint 'web-mode)
   ;; hot key for prettier-js
   (define-key web-mode-map (kbd "C-c l = =") 'prettier-js)
   ;; eval lsp after web-mode is loaded
-  ;; (eval-after-load 'web-mode
-  ;;   '(progn
-  ;;      (require 'lsp)
-  ;;      (add-hook 'web-mode-hook #'lsp-deferred)))
+  (eval-after-load 'web-mode
+    '(progn
+      ;;  (require 'lsp)
+      ;;  (add-hook 'web-mode-hook #'lsp-deferred))
+      ;; if tsx or ts file use tide
+      (when (string-match-p "tsx?\\'" (buffer-file-name))
+            (setup-tide-mode))))
 )
