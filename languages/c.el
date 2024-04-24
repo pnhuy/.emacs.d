de78993cb460a5fde788cbbeb77da22b2b6b2208
Huy Pham <keixi512@gmail.com>
2024-04-24 10:13:43 +0700

    Config for c and cpp

--- languages/c.el
+++ languages/c.el
@@ -1,6 +1,17 @@
+(use-package clang-format
+  :ensure t
+  :config
+  (setq clang-format-style "file")
+)
 ;; hook for both c and cpp mode
 (add-hook 'c-mode-common-hook
   (lambda ()
+    ;; set c style
     (setq c-default-style "bsd"
-          c-basic-offset 4))
+          c-basic-offset 4)
+    ;; enable yas minor mode
+    (yas-minor-mode)
+    ;; run lsp
+    (lsp)
+  )
 )
