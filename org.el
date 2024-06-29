79195fc82d6166ebb522185f341e4c714e4630f4
Huy Pham <keixi512@gmail.com>
2024-06-29 21:25:59 +0700

    Refactor org config

--- org.el
+++ org.el
@@ -4,17 +4,11 @@
               :type git
               :host github
               :repo "misohena/phscroll"))
-(require 'phscroll)
+;; (require 'phscroll)
 ;; uncomment the following line to enable phscroll in org-mode
 ;; (setq org-startup-truncated nil)
 ;; (with-eval-after-load "org"
 ;;   (require 'org-phscroll))
-
-(setq org-adapt-indentation nil)
-(setq org-list-allow-alphabetical t)
-
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 (defun org-table-collapse-cell ()
   (interactive)
@@ -26,44 +20,59 @@
          (org-table-check-inside-data-field))))
   (org-table-next-field))
 
-;; latex fragment preview dpi
-(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
-(setq org-latex-create-formula-image-program 'dvisvgm)
-;; (add-hook 'org-mode-hook 'org-fragtog-mode)
-
-;; active Babel languages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((R . t)
-   (python . t)))
-
-;; set path for python
-(setq org-babel-python-command "python3")
-
 ;; turn on auto-save for org-mode
 (defun org-save-all-org-buffers-quietly ()
   (save-some-buffers t (lambda () (derived-mode-p 'org-mode)))
   (when (featurep 'org-id) (org-id-locations-save)))
 
-(add-hook 'org-mode-hook (lambda ()
-                            (require 'org-tempo)                            
-                            (auto-save-mode 1)
-                            (setq auto-save-interval 50)
-                            (add-hook 'auto-save-hook 'org-save-all-org-buffers-quietly)
-                            ))
+;; function setup org-mode
+(defun set-up-org-mode ()
+  (require 'org-phscroll)
+  (setq org-adapt-indentation nil)
+  (setq org-list-allow-alphabetical t)
 
-;; turn auto-revert-mode on for org
-(add-hook 'org-mode-hook 'auto-revert-mode)
-(setq auto-revert-verbose nil)
-(setq auto-revert-use-notify nil)
+  (require 'org-bullets)
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  ;; active Babel languages
+  (org-babel-do-load-languages
+  'org-babel-load-languages
+  '((R . t)
+    (python . t)))
 
-;; enable org-tempo in org-mode
-(add-to-list 'org-modules 'org-tempo t)
+  ;; latex fragment preview dpi
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
+  (setq org-latex-create-formula-image-program 'dvisvgm)
+  
+  ;; set path for python
+  (setq org-babel-python-command "python3")
+  
+  ;; auto save
+  (require 'org-tempo)                            
+  (auto-save-mode 1)
+  (setq auto-save-interval 50)
+  (add-hook 'auto-save-hook 'org-save-all-org-buffers-quietly)
+  
+  ;; turn auto-revert-mode on for org
+  (auto-revert-mode 1)
+  (setq auto-revert-verbose nil)
+  (setq auto-revert-use-notify nil)
+  
+  ;; enable org-tempo in org-mode
+  (add-to-list 'org-modules 'org-tempo t)
+  
+  ;; disable electric-pair-mode in org-mode
+  (electric-pair-mode -1)
+  
+  ;; yasnippet for latex in org-mode
+  (yas-activate-extra-mode 'latex-mode)
+  
+  ;; enable prettify-symbols-mode in org-mode
+  (prettify-symbols-mode 1)
+  
+  ;; start startup folded
+  (setq org-startup-folded t)
+)
 
-;; disable electric-pair-mode in org-mode
-(add-hook 'org-mode-hook (lambda () (electric-pair-mode -1)))
-
-;; yasnippet for latex in org-mode
-(add-hook 'org-mode-hook (lambda () (yas-activate-extra-mode 'latex-mode)))
+(add-hook 'org-mode-hook 'set-up-org-mode)
 
 
