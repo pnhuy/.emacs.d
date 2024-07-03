7b66cefd57cadaeb2ab7803d8bdfa76098ce7772
Huy Pham <hpham@talosix.com>
2024-07-03 10:14:08 +0700

    Disable dashboard

--- dashboard.el
+++ dashboard.el
@@ -1,15 +1,24 @@
-;; use-package with package.el:
-(use-package dashboard
-  :if (< (length command-line-args) 2)
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook)
-  ;; dashboard project backend to projectile
-  (setq dashboard-projects-backend 'projectile)
-  (setq dashboard-center-content t)
-  (setq dashboard-items '((recents  . 5)
-                        ;; (bookmarks . 5)
-                        (projects . 5)
-                        ;; (agenda . 5)
-                        (registers . 5)))
-  )
+;; ;; use-package with package.el:
+;; (use-package dashboard
+;;   :if (< (length command-line-args) 2)
+;;   :ensure t
+;;   :config
+;;   (dashboard-setup-startup-hook)
+;;   ;; dashboard project backend to projectile
+;;   (setq dashboard-projects-backend 'projectile)
+;;   (setq dashboard-center-content t)
+;;   (setq dashboard-items '((recents  . 5)
+;;                         ;; (bookmarks . 5)
+;;                         (projects . 5)
+;;                         ;; (agenda . 5)
+;;                         (registers . 5)))
+;;   )
+
+;; message startup time
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
