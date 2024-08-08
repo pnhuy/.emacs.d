b538a0923a22f87ff0742f3a00a055d40ffb45d3
Huy Pham <keixi512@gmail.com>
2024-08-08 23:07:26 +0700

    chore: Update dart-mode configuration
    - Enable yas-minor-mode and my-lsp-format-buffer after saving in dart-mode
    - Set confirm-kill-processes to nil for auto killing lsp active process when exiting emacs
    - Set tab width for lsp-dart-flutter-widget-guides and lsp-dart-line-length

--- languages/dart.el
+++ languages/dart.el
@@ -9,7 +9,17 @@
 ;; Assuming usage with dart-mode
 (use-package dart-mode
   :ensure t
-  :hook (dart-mode . flutter-test-mode))
+  :hook
+  (dart-mode . yas-minor-mode)
+  (dart-mode . (lambda ()
+                 (add-hook 'after-save-hook 'my-lsp-format-buffer)))
+  :config
+  ;; auto kill lsp active process when exiting emacs
+  (setq confirm-kill-processes nil)
+  ;; tab width
+  (setq lsp-dart-flutter-widget-guides t)
+  (setq lsp-dart-line-length 120)
+)
 
 ;; (use-package flutter
 ;;   :ensure t
