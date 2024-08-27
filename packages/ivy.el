1f76ba56f1f346ddafe3833ce9aeeec86ed783b2
Huy Pham <keixi512@gmail.com>
2024-08-27 16:26:21 +0700

    Add more ivy

--- packages/ivy.el
+++ packages/ivy.el
@@ -23,7 +23,11 @@
   :init (counsel-mode 1)
   :bind (("M-x" . 'counsel-M-x)
 	 ("C-x C-f" . 'counsel-find-file)
+   ("C-x C-r" . 'counsel-recentf)
 	 ("C-x b" . 'counsel-switch-buffer))
+  :config
+  (add-to-list 'ivy-sort-functions-alist
+             '(counsel-recentf . file-newer-than-file-p))
   )
 
 (use-package ivy-prescient
@@ -34,3 +38,18 @@
   (prescient-persist-mode 1)
   (setq prescient-filter-method '(literal regexp initialism fuzzy))
   )
+
+(use-package ivy-rich
+  :ensure t
+  :after ivy
+  :config
+  (ivy-rich-mode 1)
+  )
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :after ivy-rich
+  :config
+  (all-the-icons-ivy-rich-mode 1)
+  )
+
