537d8082c6dfbe68bd59237947b639c8efad8ee7
Huy Pham <keixi512@gmail.com>
2025-04-17 06:35:45 +0300

    Add evil-org mode

--- packages/evil.el
+++ packages/evil.el
@@ -1,6 +1,14 @@
 (use-package evil
   :ensure t)
 
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
