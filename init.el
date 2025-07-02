3147b8555ffa50f05070cf524e9fd57bcd17466d
Huy Pham <keixi512@gmail.com>
2025-07-02 10:32:17 +0300

    Add mouse support for multiple cursors

--- init.el
+++ init.el
@@ -335,6 +335,8 @@
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
   )
 
 (use-package smart-hungry-delete
