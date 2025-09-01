da3c28d2374e7d8a4db32f063852ed97815e0b1e
Huy Pham <keixi512@gmail.com>
2025-09-01 22:01:42 +0300

    Refactor C/C++ mode setup

--- languages/c.el
+++ languages/c.el
@@ -4,17 +4,18 @@
   (setq clang-format-style "file")
 )
 
+;; setup function
+(defun setup-c-mode ()
+  "Setup C/C++ mode."
+  (setq c-default-style "bsd"
+        c-basic-offset 4)
+  ;; enable yas minor mode
+  (yas-minor-mode)
+  ;; run lsp
+  (lsp)
+  ;; electric mode
+  (electric-pair-mode))
+
 ;; hook for both c and cpp mode
-(add-hook 'c-mode-common-hook
-  (lambda ()
-    ;; set c style
-    (setq c-default-style "bsd"
-          c-basic-offset 4)
-    ;; enable yas minor mode
-    (yas-minor-mode)
-    ;; run lsp
-    (lsp)
-    ;; electric mode
-    (electric-pair-mode)
-  )
-)
+(add-hook 'c-mode-common-hook 'setup-c-mode)
+(add-hook 'c-ts-base-mode-hook 'setup-c-mode)
